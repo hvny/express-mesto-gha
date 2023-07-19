ad2f51517188badd19acf743775ae31f1b9e2877
Vadim Elsukov <elsukov.vadim@gmail.com>
2023-07-20 01:06:54 +0300

    fix: errors

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   console.log(name, about, avatar);
   User.create({ name, about, avatar })
-    .then((user) => res.status(201).send({ data: user }))
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные.' });
@@ -16,13 +16,19 @@ module.exports.createUser = (req, res) => {
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(201).send({ data: users }))
+    .then((users) => res.send({ data: users }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err.name}.` }));
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.id)
-    .then((user) => res.status(201).send({ data: user }))
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Пользователь не найден.' });
+      } else {
+        res.send({ data: user });
+      }
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Пользователь не найден.' });
