0b6de497cdbf736fcaabe706874c3175f553a577
Vadim Elsukov <elsukov.vadim@gmail.com>
2023-07-31 03:09:52 +0300

    test

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
+
 const {
-  createUser,
   getUsers,
   getUserById,
+  getUserInfo,
   updateProfile,
   updateAvatar,
 } = require('../controllers/users');
 
-router.post('/users', createUser);
 router.get('/users', getUsers);
 router.get('/users/:userId', getUserById);
+router.get('/users/me', getUserInfo);
 router.patch('/users/me', updateProfile);
 router.patch('/users/me/avatar', updateAvatar);
 
