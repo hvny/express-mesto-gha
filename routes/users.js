e1ceb934d087c29b855b5eaff291e95e675331a9
Vadim Elsukov <elsukov.vadim@gmail.com>
2023-07-31 05:23:08 +0300

    another commit

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get('/users', getUsers);
 
 router.get('/users/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().required().hex(),
+    userId: Joi.string().hex().required(),
   }),
 }), getUserById);
 
